a256033879f5aa93aca8795f5ebda1387ca4103d
Maeve Potter <potter.maeve@gmail.com>
2017-04-25 11:14:28 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+w = `
+h
+y
+`;(
+Perhaps => {
+    Perhaps.some = {}
+
+    Perhaps.some["patience"] = will => /do?/;
+    Perhaps.some["Doubts"]   = {
+                    will_give: 
+                        new Promise(s => [() => 
+
+                !![] << 0 ^ 0 >> !![]
+                         ,w,
+
+    ])}; return {to: "the"}})( window )
+                                    .I = "rest";
+
+(/                      UNREST                      /)
+
+        ;;;;;;; ;;;                 ;;;;;;;;;;;;;   
+        ;"you"; let     space       = "happened";       
+        ;;;;;;; ;;;                 ;;;;;;;;;;;;;   
+
+with (some) 
+    patience ([`I
+                pondered what's`] |
+        true    +
+
+Doubts  + [
+    ..."to be"].reduce      (
+        (however, still) => {linger : "         "}))
